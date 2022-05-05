cf9e213d81af744a9fb2a448124eb4f4160b15a3
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2022-05-05 18:40:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+img="";
+Status="";
+objects=[];
+function preload(){ 
+  img = loadImage('dog_cat.jpg'); 
+} 
+function setup() { 
+  canvas = createCanvas(640, 420); 
+  canvas.center(); 
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML="Status = Detecting Objects";
+} 
+function draw() { 
+  image(img, 0, 0, 640, 420); 
+  
+  if(Status != ""){
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML="Status : Objects Detected";
+
+      fill("#0000FF");
+      percent=floor(objects[i].confidence*100);
+      text(objects[i].label+" "+ percent +"%", objects[i].x + 15, objects[i].y+15);
+      noFill();
+      stroke(0,255,0);
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      noStroke();
+    }
+  }
+}
+function modelLoaded() {
+  console.log("Yay!!!!! Riya is happy that the model is loaded!!");
+  Status=true;
+  objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+if(error){
+  console.log(error);
+}
+console.log(results);
+objects = results;
+}
